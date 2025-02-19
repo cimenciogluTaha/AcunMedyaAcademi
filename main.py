cbdd51ac57d66a395085889ab943007180008dd4
Taha <77041808+cimenciogluTaha@users.noreply.github.com>
2025-02-19 18:15:39 +0300

    main.py

--- main.py
+++ main.py
@@ -1,74 +1,114 @@
-age = 25
-print(age)
+print("Kodlamaio")
+baslik = "Taşıt Kredisi"
+print(baslik)
+#string => metinsel ifade
+baslik = "İhtiyaç Kredisi"
+print(baslik)
+#int, integer => tam sayı
+vade = 36
+ekVade = 6
+vade2 = "36"
 
-age = 30
-print(age)
+# float, decimal, double
+aylikOdeme = 200.50 
+
+# bool, boolean => True veya False
+yukselisteMi = False
+
+# matematiksel operatörler
+
+# + 
+print(5 + 5)
+print(vade + 12)
+print(vade + ekVade)
+print(36 + 6)
+
+# -
+print(5-5)
+print(vade-12)
+print(vade - ekVade)
+print(36-6)
+
+# *
+print(5*5)
+print(vade * 2)
+print(vade * ekVade)
+print(10*10)
+
+# /
+print(5/5)
+print(vade / 2)
+print(vade / ekVade)
+print(10/2)
 
 
-name = 'Taha'
-print(name)
-print( type(name) )
+yeniVade = vade / 2
+fiyat = 100
+indirimliFiyat = fiyat - 20
 
-name = 30
-print(name)
-print( type(name) )
+print(yeniVade)
+print(indirimliFiyat)
 
-
-
-print(1+1)
-print(10-5)
-print(5*10)
-
-print(25/5)
-print(25//5)
-print(27//5)
-
-print(100%3)
-print(5**3)
-
-print(type(25/5))
-#
-
-a = 1
-a += 5
-a -= 3
-a *= 2
-a //= 1
-print(a)
+# % => mod operator
+print(10%2)
+print(vade % 5)
+print(vade % ekVade)
+print(30 % 10)
 
 
-is_valid = True
+# mantıksal operatörler
 print(1 == 1)
 print(1 == 2)
-print(3 != 3)
-#
-print(10 > 5)
-print(10 >= 5)
-#
-print(5 < 10)
-print(5 <= 10)
-#
-print('Taha' == 'Taha')
 
-print('car' > 'Bus')
 #
+print(1 > 2)
+print(3 > 1)
+print(1 > 1)
+print(1 >= 1)
 
-print( 1==1 and 10 > 5 )
-print( 1==2 and 10 > 5 )
-#
-print( 1==1 or 10 > 5 )
-print( 1==2 or 10 > 5 )
-print( 1==2 or 10 < 5  and 1==1)
-#
+print(1 < 2)
+print(3 < 1)
+print(1 < 1)
+print(1 <= 1)
 
-print( 1==1 and 5<3 or 10==10 and 5>=6 )
 
-print( ( 1==1 and 2>5 ) or 6>=6 and (10>5 or 5== 5 ) )
+print(1 != 1)
+print(1 != 2)
 
-print(not 1==1)
-#
+# or and
 
-sayı1 = 10
-sayı2 = 20
+#or => veya
+print(1 > 2 or 5 > 2)
+print(1 > 2 and 5 > 2)
+print(1 > 2 or 5 > 2 and 3 > 2)
 
-if sayı1 > sayı2:
+print(1 > 2 and 5 > 2 and 3 > 2)
+
+print(2 > 1 or 1 > 2 and 3 > 2)
+
+
+# karar yapıları
+# if else 
+
+#condition
+sayi1 = 15
+sayi2 = 15
+#indent
+if sayi1 <= sayi2:
+    print("Sayi 1 Sayi 2'den küçüktür")
+elif sayi1 == sayi2:
+    print("İki sayı eşittir")
+#eğer if ve else if bloklarında hiç birine girmez ise
+else:
+    print("Sayi 1 Sayi 2'den büyüktür")
+
+print("******************")
+
+if sayi1 <= sayi2:
+    print("Sayi 1 Sayi 2'den küçüktür")
+if sayi1 == sayi2:
+    print("İki sayi eşittir")
+else:
+    print("Sayi 1 Sayi 2'den büyüktür")
+
+print("Burası if bloğunun dışıdır.")
