26b01f1733e799e7290fbe5d5685031626d10739
Taha <77041808+cimenciogluTaha@users.noreply.github.com>
2025-02-19 18:17:00 +0300

    Modules.py

---
+++ Modules.py
@@ -0,0 +1,10 @@
+from matematik import topla as toplamaIslemi
+from Day2 import sayHello
+from Human import Human # type: ignore
+import random
+print(toplamaIslemi(10,20))
+
+print(random.randint(0,100))
+
+human1 = Human("Mirza")
+human1.talk("Merhaba")
