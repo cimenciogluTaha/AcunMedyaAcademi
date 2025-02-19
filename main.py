c8a43d6037027165300cfd3cad7215ddc3b980f8
Taha <77041808+cimenciogluTaha@users.noreply.github.com>
2025-02-19 17:12:33 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,74 @@
+age = 25
+print(age)
+
+age = 30
+print(age)
+
+
+name = 'Taha'
+print(name)
+print( type(name) )
+
+name = 30
+print(name)
+print( type(name) )
+
+
+
+print(1+1)
+print(10-5)
+print(5*10)
+
+print(25/5)
+print(25//5)
+print(27//5)
+
+print(100%3)
+print(5**3)
+
+print(type(25/5))
+#
+
+a = 1
+a += 5
+a -= 3
+a *= 2
+a //= 1
+print(a)
+
+
+is_valid = True
+print(1 == 1)
+print(1 == 2)
+print(3 != 3)
+#
+print(10 > 5)
+print(10 >= 5)
+#
+print(5 < 10)
+print(5 <= 10)
+#
+print('Taha' == 'Taha')
+
+print('car' > 'Bus')
+#
+
+print( 1==1 and 10 > 5 )
+print( 1==2 and 10 > 5 )
+#
+print( 1==1 or 10 > 5 )
+print( 1==2 or 10 > 5 )
+print( 1==2 or 10 < 5  and 1==1)
+#
+
+print( 1==1 and 5<3 or 10==10 and 5>=6 )
+
+print( ( 1==1 and 2>5 ) or 6>=6 and (10>5 or 5== 5 ) )
+
+print(not 1==1)
+#
+
+sayı1 = 10
+sayı2 = 20
+
+if sayı1 > sayı2:
